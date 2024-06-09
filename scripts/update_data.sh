a8ccfb328a56d665f0ada1704bbab3fc4cdc2c2a
Rafay Khan <82721722+rafaykhan-source@users.noreply.github.com>
2024-06-09 13:49:44 -0400

    Simplify update_data.sh

--- scripts/update_data.sh
+++ scripts/update_data.sh
@@ -1,3 +1,3 @@
-rm -vrf ~/Projects/marshmallow/data
-cp -r ~/Projects/student-datapipelines/processed-data ~/Projects/marshmallow
-mv ~/Projects/marshmallow/processed-data ~/Projects/marshmallow/data
+# run in marshmallow directory
+rm -vrf data
+cp -r ~/Projects/student-datapipelines/processed-data data
