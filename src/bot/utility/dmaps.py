d35c8bd99412495fb7f6ace197133b17a6a14cb1
Rafay Khan <82721722+rafaykhan-source@users.noreply.github.com>
2024-07-16 23:23:12 -0400

    Add dmaps.py

---
+++ src/bot/utility/dmaps.py
@@ -0,0 +1,54 @@
+"""The dmaps module is responsible for producing and constructing maps for discord objects."""  # noqa: E501
+
+import logging
+
+import discord
+from discord.ext import commands
+
+logger = logging.getLogger("utility")
+
+
+async def get_channel_map(
+    ctx: commands.Context, channels: list[str]
+) -> dict[str, discord.abc.GuildChannel]:
+    """Returns a map between channel names and their corresponding discord object.
+
+    Args:
+        ctx (commands.Context): The command context object.
+        channels (list[str]): The desired channels.
+
+    Returns:
+        dict[str, discord.GuildChannel]: The channel object mapping.
+    """
+    channel_map = {}
+    for channel in channels:
+        guild_channel = await discord.ext.commands.GuildChannelConverter().convert(
+            ctx, channel
+        )
+        if not guild_channel:
+            logger.warning("No associated guild channel with %s.", channel)
+        channel_map[channel] = guild_channel
+
+    return channel_map
+
+
+async def get_role_map(
+    ctx: commands.Context, roles: list[str]
+) -> dict[str, discord.Role]:
+    """Returns a map between role names and their corresponding discord object.
+
+    Args:
+        ctx (commands.Context): The command context object.
+        roles (list[str]): The desired roles.
+
+    Returns:
+        dict[str, discord.GuildChannel]: The role object mapping.
+    """
+    role_map = {}
+    for role in roles:
+        guild_role = await discord.ext.commands.RoleConverter().convert(ctx, role)
+        if not guild_role:
+            logger.warning("No associated guild role with %s.", role)
+        role_map[role] = guild_role
+
+    return role_map
