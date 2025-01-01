8f3ee4a0532c12cad217f7dc3fa580199cdd6cf7
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2025-01-01 01:33:18 -0500

    refactor: revamp logging

--- src/marshmallow/bot.py
+++ src/marshmallow/bot.py
@@ -1,24 +1,10 @@
-"""A class representing a MarshmallowBotClient.
-
-This module defines a class representing Marshmallow bot, which
-supports commands/operations relevant for performing/automating
-administrative and managerial tasks on the Princeton Emma
-Bloomberg Center Discords. The MarshmallowBotClient is a
-subclass of discord.py's commands.Bot.
-
-Typical usage example:
-
-  bot = MarshmallowBotClient()
-  bot.run(token=TOKEN, log_handler=None)
-"""
+"""A class representing a MarshmallowBotClient."""
 
 import logging
 
 from discord.ext import commands
 
 import marshmallow.settings as stg
-
-logger = logging.getLogger(__name__)
 
 
 class MarshmallowBotClient(commands.Bot):
@@ -31,6 +17,7 @@ class MarshmallowBotClient(commands.Bot):
             intents=stg.get_intents(),
             activity=stg.get_random_discord_activity(),
         )
+        self.logger = logging.getLogger(__name__)
 
     async def setup_hook(self) -> None:
         """Loads bot extensions."""
@@ -38,7 +25,7 @@ class MarshmallowBotClient(commands.Bot):
 
         for cog_name in cog_names:
             await self.load_extension(f"extensions.{cog_name}")
-            logger.info("Loaded Cog: %s", cog_name)
+            self.logger.info("Loaded Cog: %s", cog_name)
 
         return await super().setup_hook()
 
@@ -46,7 +33,7 @@ class MarshmallowBotClient(commands.Bot):
         """Event called upon successful login and loaded data."""
         if self.user:
             print(f"Logged in as {self.user} (ID: {self.user.id})")
-            logger.info("Logged in as %s (ID: %s)", self.user, self.user.id)
+            self.logger.info("Logged in as %s (ID: %s)", self.user, self.user.id)
 
 
 if __name__ == "__main__":
