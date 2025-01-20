b1fb8803ccf36ec3c7bf30be23dbdd0bcf60ba9c
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2025-01-19 20:53:07 -0500

    refactor: move logging into main

--- src/marshmallow/__main__.py
+++ src/marshmallow/__main__.py
@@ -7,18 +7,20 @@ import logging.config
 import marshmallow.settings as stg
 from marshmallow.bot import MarshmallowBotClient
 
-logger = logging.getLogger("run")
-
 
 async def main() -> None:
     """Runs Marshmallow."""
+    logger = logging.getLogger("run")
+
     logger.info("Instantiating Marshmallow Bot Client.")
     marsh = MarshmallowBotClient()
 
+    logger.info("Retrieving Token.")
+    token = stg.get_token()
+
     async with marsh as bot:
-        logger.info("Retrieving Token.")
-        await bot.start(stg.get_token())
         logger.info("Starting Marshmallow.")
+        await bot.start(token)
 
 
 asyncio.run(main())
--- src/marshmallow/bot.py
+++ src/marshmallow/bot.py
@@ -5,11 +5,6 @@ supports commands/operations relevant for performing/automating
 administrative and managerial tasks on the Princeton Emma
 Bloomberg Center Discords. The MarshmallowBotClient is a
 subclass of discord.py's commands.Bot.
-
-Typical usage example:
-
-  bot = MarshmallowBotClient()
-  bot.run(token=TOKEN, log_handler=None)
 """
 
 import logging
@@ -18,14 +13,13 @@ from discord.ext import commands
 
 import marshmallow.settings as stg
 
-logger = logging.getLogger(__name__)
-
 
 class MarshmallowBotClient(commands.Bot):
     """This class contains behavior unique to Marshmallow."""
 
     def __init__(self) -> None:
         """Instantiates the bot client."""
+        self.logger = logging.getLogger(__name__)
         super().__init__(
             command_prefix=stg.get_command_prefix(),
             intents=stg.get_intents(),
@@ -38,7 +32,7 @@ class MarshmallowBotClient(commands.Bot):
 
         for cog_name in cog_names:
             await self.load_extension(f"extensions.{cog_name}")
-            logger.info("Loaded Cog: %s", cog_name)
+            self.logger.info("Loaded Cog: %s", cog_name)
 
         return await super().setup_hook()
 
@@ -46,7 +40,7 @@ class MarshmallowBotClient(commands.Bot):
         """Event called upon successful login and loaded data."""
         if self.user:
             print(f"Logged in as {self.user} (ID: {self.user.id})")
-            logger.info("Logged in as %s (ID: %s)", self.user, self.user.id)
+            self.logger.info("Logged in as %s (ID: %s)", self.user, self.user.id)
 
 
 if __name__ == "__main__":
